20ce68fcde8d8992e523d6d526209563ebd8508a
Oscar Keur <o.e.keur@gmail.com>
2019-08-06 16:25:53 +0200

    Add processing of login

--- includes/assets/js/src/gpnl-inforequest.js
+++ includes/assets/js/src/gpnl-inforequest.js
@@ -1,4 +1,5 @@
 var request_form_element = {};
+var check_form_element = {};
 $('.hideshowbtn').on('click', function () {
   let btn = $(this);
   let form  = btn.data('target');
@@ -7,9 +8,7 @@ $('.hideshowbtn').on('click', function () {
 });
 
 $('.gpnl-request__form').on('submit', function () {
-
   request_form_element = this;
-
   var post_form_value = getFormObj(request_form_element);
   var form_config = 'request_form_object';
   post_form_value.action = 'request_form_process';
@@ -30,24 +29,42 @@ $('.gpnl-request__form').on('submit', function () {
       $(request_form_element).find('*').hide();
       $(request_form_element).append('<h2>Hoera, je bent er bijna!</h2>');
 
-      // // Send conversion event to the GTM
-      // if (typeof dataLayer !== 'undefined') {
-      //   dataLayer.push({
-      //     'event'         : 'nieuwsbriefformulier',
-      //     'conv_campaign' : post_form_value.literaturecode,
-      //     'conv_action'   : 'registreer',
-      //     'conv_label'    : 'registreer-nieuwsbrief'
-      //   });
-      // }
-
     },
     error: function(){
       // If the backend sends an error, hide the thank element and show an error urging to try again
       showErrorMessage(request_form_element);
     }
   });
+});
 
+$('.gpnl-check__form').on('submit', function () {
+  check_form_element = this;
+  var post_form_value = getFormObj(check_form_element);
+  var form_config = 'request_form_object';
+  post_form_value.action = 'check_form_process';
+  post_form_value.nonce  = window[form_config].nonce;
 
+  toggleDisable($(check_form_element).find('*'));
+  if (post_form_value.human !== '') {
+    showErrorMessage(check_form_element);
+    return;
+  }
+
+  $.ajax({
+    type:    'POST',
+    url:     window[form_config].ajaxUrl,
+    data:    post_form_value,
+    success: function(data) {
+      $(check_form_element).find('*').hide();
+      $(check_form_element).append('<h2>Hoera, je bent er bijna!</h2>');
+      console.log(data);
+
+    },
+    error: function(){
+      // If the backend sends an error, hide the thank element and show an error urging to try again
+      showErrorMessage(check_form_element);
+    }
+  });
 });
 
 // Get the key+value from the input fields in the form
