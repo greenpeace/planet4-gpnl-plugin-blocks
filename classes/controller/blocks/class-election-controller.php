d6ee1a72cb652b5f7da3f10af8eba74c7c47a531
Oscar Keur <o.e.keur@gmail.com>
2019-08-21 22:38:21 +0200

    Style slick arrows + dots

--- classes/controller/blocks/class-election-controller.php
+++ classes/controller/blocks/class-election-controller.php
@@ -151,8 +151,8 @@ if ( ! class_exists( 'GPNL_Election_Controller' ) ) {
 		public function prepare_template( $fields, $content, $shortcode_tag ) : string {
 
 			wp_enqueue_style( 'gpnl_education_css', P4NLBKS_ASSETS_DIR . 'css/gpnl-educationcovers.css', [], '2.4.0' );
-			wp_enqueue_style( 'gpnl_election_css', P4NLBKS_ASSETS_DIR . 'css/gpnl-election.css', [], '2.4.0' );
 			wp_enqueue_style( 'gpnl_slick_css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css', [ 'slick' ], '2.4.0' );
+			wp_enqueue_style( 'gpnl_election_css', P4NLBKS_ASSETS_DIR . 'css/gpnl-election.css', [], '2.4.0' );
 			wp_enqueue_script( 'gpnl_election_js', P4NLBKS_ASSETS_DIR . 'js/gpnl-election.js', [ 'jquery', 'slick' ], '2.4.0', true );
 			// Enqueue the script:
 			wp_enqueue_script( 'jquery-docready-script', P4NLBKS_ASSETS_DIR . 'js/onsubmit.js', [ 'jquery' ], '2.9.1', true );
