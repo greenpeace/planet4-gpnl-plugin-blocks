feeb5129ee76f6a5ba6d78032dba10a222d45126
Oscar Keur <o.e.keur@gmail.com>
2019-08-22 09:01:26 +0200

    Add scaffolding for thank message

--- classes/controller/blocks/class-election-controller.php
+++ classes/controller/blocks/class-election-controller.php
@@ -40,6 +40,12 @@ if ( ! class_exists( 'GPNL_Election_Controller' ) ) {
 					'frameTitle'  => __( 'Selecteer afbeelding', 'planet4-blocks-backend' ),
 				],
 				[
+					'label' => __( 'Bedankt tekst', 'planet4-gpnl-blocks' ),
+					'attr'  => 'thanktext',
+					'type'  => 'textarea',
+					'value' => '',
+				],
+				[
 					'label' => __( 'Opt in tekst', 'planet4-gpnl-blocks' ),
 					'attr'  => 'consent',
 					'type'  => 'textarea',
@@ -153,12 +159,15 @@ if ( ! class_exists( 'GPNL_Election_Controller' ) ) {
 			wp_enqueue_style( 'gpnl_education_css', P4NLBKS_ASSETS_DIR . 'css/gpnl-educationcovers.css', [], '2.11.0-pre' );
 			wp_enqueue_style( 'gpnl_slick_css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css', [ 'slick' ], '2.11.0-pre' );
 			wp_enqueue_style( 'gpnl_election_css', P4NLBKS_ASSETS_DIR . 'css/gpnl-election.css', [], '2.11.0-pre' );
+			wp_enqueue_style( 'gpnl_petition_css', P4NLBKS_ASSETS_DIR . 'css/gpnl-petition.css', [], '2.3.6' );
+
 			wp_enqueue_script( 'gpnl_election_js', P4NLBKS_ASSETS_DIR . 'js/gpnl-election.js', [ 'jquery', 'slick' ], '2.11.0-pre', true );
 			// Enqueue the script:
 			wp_enqueue_script( 'jquery-docready-script', P4NLBKS_ASSETS_DIR . 'js/onsubmit.js', [ 'jquery' ], '2.11.0-pre', true );
 			$attributes = [
 				'title'           => '',
 				'backgroundimage' => '',
+				'thanktext'       => '',
 				'consent'         => '',
 				'campaignpolicy'  => '',
 				'literaturecode'  => '',
