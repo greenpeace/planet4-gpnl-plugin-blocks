5387f9cb3313cd0d17a31ce4f8b2591f5970418c
Oscar Keur <o.e.keur@gmail.com>
2019-09-13 16:14:12 +0200

    up version for release

--- classes/controller/blocks/class-petition-controller.php
+++ classes/controller/blocks/class-petition-controller.php
@@ -314,7 +314,7 @@ if ( ! class_exists( 'Petition_Controller' ) ) {
 			}
 
 			// Include the script and styling for the counter
-			wp_enqueue_script( 'petitioncounterjs', P4NLBKS_ASSETS_DIR . 'js/onload.js', [ 'jquery', 'jquery-effects-core' ], '2.6.9', true );
+			wp_enqueue_script( 'petitioncounterjs', P4NLBKS_ASSETS_DIR . 'js/onload.js', [ 'jquery', 'jquery-effects-core' ], '2.13.0', true );
 			wp_enqueue_style( 'petitioncountercss', P4NLBKS_ASSETS_DIR . 'css/gpnl-petition.css', [], '2.11.4' );
 
 			/*
